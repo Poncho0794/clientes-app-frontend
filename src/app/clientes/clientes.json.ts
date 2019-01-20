54847dce2e6f05e578735e11922291073b061e33
Alfonso <benbanda07@gmail.com>
2019-01-19 20:10:16 -0600

    Init Angular APP

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Alfonso', apellido: 'Banda', email: 'benbanda07@gmail.com', createAt: '2019-01-19' },
+  { id: 2, nombre: 'Benjamin', apellido: 'Reyna', email: 'poncho@gmail.com', createAt: '2019-01-19' },
+  { id: 3, nombre: 'Juan', apellido: 'Ortiz', email: 'juanjo@gmail.com', createAt: '2019-01-19' },
+  { id: 4, nombre: 'Kenneth', apellido: 'Martinez', email: 'kenMtz@gmail.com', createAt: '2019-01-19' },
+  { id: 5, nombre: 'Valeria', apellido: 'Valades', email: 'val.val@gmail.com', createAt: '2019-01-19' },
+  { id: 6, nombre: 'Mariana', apellido: 'Treviño', email: 'mar.trev@gmail.com', createAt: '2019-01-19' },
+];
