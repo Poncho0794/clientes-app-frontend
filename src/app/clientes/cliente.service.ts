a27586f5ff6003590e833b22c00f9b228ee36a75
Alfonso <benbanda07@gmail.com>
2019-01-20 01:25:57 -0600

    Add create form and service

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from "@angular/core";
-import { CLIENTES } from "./clientes.json";
 import { Cliente } from "./cliente";
-import { Observable, of } from "rxjs";
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators'
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Injectable()
 export class ClienteService {
-  private urlEndpoint: string = 'http://localhost:8080/api/clientes'
+  private urlEndpoint: string = "http://localhost:8080/api/clientes";
+
+  private httpHeaders = new HttpHeaders({ "Content-Type": "application/json" });
+
   constructor(private http: HttpClient) {}
 
   getClientes(): Observable<Cliente[]> {
     //return of(CLIENTES);
-    return this.http.get(this.urlEndpoint).pipe(
-      map(response => response as Cliente[])
-    )
+    //Manera 1 de castear el response
+    return this.http
+      .get(this.urlEndpoint)
+      .pipe(map(response => response as Cliente[]));
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    //manera 2 de castear el response
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, {
+      headers: this.httpHeaders
+    });
   }
 }
---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { Cliente } from "./cliente";
+import { ClienteService } from "./cliente.service";
+
+import { Router } from "@angular/router";
+@Component({
+  selector: "app-form",
+  templateUrl: "./form.component.html"
+})
+export class FormComponent implements OnInit {
+  private cliente: Cliente = new Cliente();
+  private titulo: string = "Crear Cliente";
+  constructor(private clienteService: ClienteService, private router: Router) {}
+
+  ngOnInit() {}
+
+  public create(): void {
+    console.log("Clicked");
+    console.log(this.cliente);
+    this.clienteService.create(this.cliente).subscribe(
+      //Redirigir al listado de clientes cuando responda
+      response => this.router.navigate(["/clientes"])
+    );
+  }
+}
